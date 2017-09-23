388e24047db7b5375b8a4eb44f23956a2894f73f
gundralaa <mathgame99@gmail.com>
2017-09-23 15:48:28 -0700

    working recycler view with fake data

--- BluBrary/app/src/main/java/com/example/android/blubrary/BookListAdapter.java
+++ BluBrary/app/src/main/java/com/example/android/blubrary/BookListAdapter.java
@@ -12,10 +12,12 @@ import android.widget.TextView;
  */
 
 public class BookListAdapter extends RecyclerView.Adapter<BookListAdapter.BookListViewHolder> {
+    private static Book [] books = new Book[5];
 
-    Book book1 = new Book(null, null,"The Scarlet Letter", "Nathanial Hawthorne", null, null, null);
-    Book [] books = {book1};
-    public BookListAdapter(){}
+    public BookListAdapter(){
+        Book book1 = new Book("12534dda", "99" ,"The Scarlet Letter", "Nathanial Hawthorne", ("nonfiction"),"lol" , "9");
+        books[0] = book1;
+    }
 
     @Override
     public BookListViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -29,14 +31,20 @@ public class BookListAdapter extends RecyclerView.Adapter<BookListAdapter.BookLi
 
     @Override
     public void onBindViewHolder(BookListViewHolder holder, int position) {
-        holder.mAuthorName.setText(books[position].getAuthor());
-        holder.mBookTitle.setText(books[position].getTitle());
+        if(books[position] != null) {
+            holder.mAuthorName.setText(books[position].getAuthor());
+            holder.mBookTitle.setText(books[position].getTitle());
+        }
     }
 
     @Override
     public int getItemCount() {
         if(null == books) return 0;
         return books.length;
+    }
+    public void writeToArray(){
+        Book book1 = new Book("12534dda", "99" ,"The Scarlet Letter", "Nathanial Hawthorne", ("nonfiction"),"lol" , "9");
+        books[0] = book1;
     }
 
     class BookListViewHolder extends RecyclerView.ViewHolder{
@@ -47,7 +55,7 @@ public class BookListAdapter extends RecyclerView.Adapter<BookListAdapter.BookLi
         public BookListViewHolder(View view){
             super(view);
             mBookTitle = (TextView) view.findViewById(R.id.tv_book_name);
-            mAuthorName = (TextView) view.findViewById(R.id.tv_author_name);
+            mAuthorName = (TextView) view.findViewById(R.id.tv_book_author);
         }
 
     }
--- BluBrary/app/src/main/java/com/example/android/blubrary/MainActivity.java
+++ BluBrary/app/src/main/java/com/example/android/blubrary/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.android.blubrary;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -15,6 +17,8 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    private RecyclerView mRecyclerView;
+    private BookListAdapter mBookListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +44,17 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycle_view_books);
+
+        LinearLayoutManager layoutManager
+                = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+
+        mRecyclerView.setLayoutManager(layoutManager);
+
+        mBookListAdapter = new BookListAdapter();
+
+        mRecyclerView.setAdapter(mBookListAdapter);
     }
 
     @Override
