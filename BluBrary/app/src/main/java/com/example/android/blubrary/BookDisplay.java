b75455a7b025b7173970f3dd00bbb89e66f18661
Danny <saelid2517@gmail.com>
2017-10-04 19:04:47 -0700

    All except images figured out

--- BluBrary/app/src/main/java/com/example/android/blubrary/BookDisplay.java
+++ BluBrary/app/src/main/java/com/example/android/blubrary/BookDisplay.java
@@ -7,12 +7,16 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.ImageView;
 
 public class BookDisplay extends AppCompatActivity {
 
     TextView mTitleView;
     TextView mAuthorView;
     TextView mGenreView;
+    TextView mCallView;
+    TextView mShelfView;
+    ImageView mCoverArt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +27,8 @@ public class BookDisplay extends AppCompatActivity {
         mTitleView = (TextView) findViewById(R.id.tv_book_name_more);
         mAuthorView = (TextView) findViewById(R.id.tv_book_author_more);
         mGenreView = (TextView) findViewById(R.id.tv_book_genre_more);
+        mCallView = (TextView) findViewById(R.id.tv_book_callNum_more);
+        mShelfView = (TextView) findViewById(R.id.tv_book_shelfNum_more);
         Intent startingIntent = getIntent();
 
         if (startingIntent.hasExtra("BookPosition")) {
@@ -30,11 +36,22 @@ public class BookDisplay extends AppCompatActivity {
             final Book book = MainActivity.library[bookPosition];
             final String title = book.getTitle();
             String author = book.getAuthor();
-            String genre = (book.getGenres())[0];
-            
+            String genre = "";
+            String[] genres = book.getGenres();
+            for (int i=0; i<genres.length; i++) {
+                if (i==genres.length-1) {
+                    genre += genres[i];
+                }
+                else {
+                    genre += (genres[i] + ", ");
+                }
+            }
+
             mTitleView.setText(title);
             mAuthorView.setText(author);
-            mGenreView.setText(genre);
+            mGenreView.setText(("Genre(s): " + genre));
+            mCallView.setText(("Call Number: " + book.getCallNumber()));
+            mShelfView.setText(("Shelf # " + book.getShelfNumber()));
 
         }
 
